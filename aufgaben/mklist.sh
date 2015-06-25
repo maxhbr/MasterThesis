05eb1851b20137b1a36acdcbc014ffa337667b44
maximilianhuber <mail@maximilian-huber.de>
2015-06-25 17:09:54 +0200

    weiter

--- aufgaben/mklist.sh
+++ aufgaben/mklist.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 cd "`dirname "$0"`"
 
-echo "% generated with mklist.sh" > ../aufgaben.tex
-echo "\\chapter{Aufgaben und Fragen}" >> ../aufgaben.tex
+echo "% generated with mklist.sh" > ../app-aufgaben.tex
+echo "\\chapter{Aufgaben und Fragen}" >> ../app-aufgaben.tex
 
 for f in ./*.tex
 do
-  echo "\\input{aufgaben/${f##*/}}" >> ../aufgaben.tex
+  echo "\\input{aufgaben/${f##*/}}" >> ../app-aufgaben.tex
 done
 
-cat ../aufgaben.tex
+cat ../app-aufgaben.tex
--- notes/mklist.sh
+++ notes/mklist.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 cd "`dirname "$0"`"
 
-echo "% generated with mklist.sh" > ../notes.tex
-echo "\\chapter{Notes}" >> ../notes.tex
+echo "% generated with mklist.sh" > ../app-notes.tex
+echo "\\chapter{Notes}" >> ../app-notes.tex
 
 for f in ./*.tex
 do
-  echo "\\input{notes/${f##*/}}" >> ../notes.tex
+  echo "\\input{notes/${f##*/}}" >> ../app-notes.tex
 done
 
-cat ../notes.tex
+cat ../app-notes.tex
